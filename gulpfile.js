9c30f9927fb8a3dcb902dff377b99b21bc9c8c98
lpmi-13 <leskis@gmail.com>
2019-07-04 14:07:47 +0100

    add basic gulp build process
    now with moar python!

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const inject = require('gulp-inject-string');
+const terser= require('gulp-terser');
+const rename = require('gulp-rename');
+const stripDebug = require('gulp-strip-debug');
+
+gulp.task('miniJS', done => {
+  gulp.src('./drawing.js')
+    // replace file paths for assets
+    .pipe(stripDebug())
+    .pipe(terser({
+      mangle: true
+    }))
+    .pipe(rename('drawing.min.js'))
+    .pipe(gulp.dest('dist/'))
+  done();
+});
+
+gulp.task('html', done => {
+  gulp.src('index.html')
+    .pipe(inject.replace(
+      'drawing.js',
+      'drawing.min.js'
+    ))
+    .pipe(gulp.dest('dist/'))
+  done();
+});
+
+gulp.task('default', gulp.parallel(
+  'miniJS',
+  'html',
+  )
+);
