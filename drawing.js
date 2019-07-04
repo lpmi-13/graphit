e956202dcf71c75825d33f9ed1d237a5f3e9f6fd
lpmi-13 <leskis@gmail.com>
2019-07-04 07:49:18 +0100

    initial commit

---
+++ drawing.js
@@ -0,0 +1,82 @@
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
+
+canvas.setAttribute('width', window.innerWidth);
+canvas.setAttribute('height', window.innerHeight);
+
+
+var x, y, isPainting;
+var origX, origY, finalX, finalY;
+
+function getCoordinates(event) {
+  if (['mousedown', 'mousemove'].includes(event.type)) {
+    return [event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop];
+  } else {
+    return [
+      event.touches[0].pageX - canvas.offsetLeft,
+      event.touches[0].pageY - canvas.offsetTop
+    ];
+  }
+}
+
+function startPaint(e) {
+  isPainting = true;
+  var coordinates = getCoordinates(e);
+  origX = x = coordinates[0];
+  origY = y = coordinates[1];
+}
+
+function drawLine(firstX, firstY, secondX, secondY) {
+  context.strokeStyle = "black";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+
+  context.beginPath();
+  context.moveTo(secondX, secondY);
+  context.lineTo(firstX, firstY);
+  context.closePath();
+
+  // actually draw the path
+  context.stroke();
+}
+
+function paint(e) {
+  if (isPainting) {
+    var [newX, newY] = getCoordinates(e);
+    drawLine(x, y, newX, newY);
+
+    // set x and y to our new coordinates
+    finalX = x = newX;
+    finalY = y = newY;
+  }
+}
+
+function getSlope(startX, startY, endX, endY) {
+  var slope = (startY - endY) / (endX - startX);
+  console.log('starting X is', startX);
+  console.log('starting Y is', startY);
+  console.log('final X is', endX);
+  console.log('final Y is', endY);
+  console.log('the slope is ', slope.toFixed(2));
+}
+
+function exit() {
+  isPainting = false;
+  getSlope(origX, origY, finalX, finalY);
+}
+
+function clearAll() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas.addEventListener('mousedown', startPaint);
+canvas.addEventListener('touchstart', startPaint);
+canvas.addEventListener('mousedown', clearAll);
+canvas.addEventListener('touchstart', clearAll);
+
+canvas.addEventListener('mousemove', paint);
+canvas.addEventListener('touchmove', paint);
+
+canvas.addEventListener('mouseup', exit);
+canvas.addEventListener('mouseleave', exit);
+canvas.addEventListener('touchend', exit);
