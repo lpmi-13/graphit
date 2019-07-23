9b03f852513a580e9247a8c9fa45fc0ab1cf9746
lpmi-13 <leskis@gmail.com>
2019-07-23 18:17:48 +0100

    current work for version 2.0

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 import regression from 'regression';
 
+// hack to keep the document from even attempting to scroll
 const noScroll = document.querySelector('body');
 noScroll.addEventListener('touchmove', function(e) {
   e.preventDefault();
 }, {passive: false });
 
-const result = regression.linear([[0, 1], [32, 67], [12, 79]]);
-console.log(result);
-
-
+// set up our canvas
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+canvas.setAttribute('width', window.innerWidth);
+canvas.setAttribute('height', window.innerHeight);
 
 const fontRatio = 80 / 1000;
 const fontSize = window.innerWidth * fontRatio;
 
-const coordinateArray = [];
+// these set our artificial centerpoints instead of the top left corner
+const fakeCenterX = canvas.width/2;
+const fakeCenterY = canvas.height/2;
 
-canvas.setAttribute('width', window.innerWidth);
-canvas.setAttribute('height', window.innerHeight);
+let coordinateArray = [];
 
-
-var x, y, isPainting;
-var origX, origY, finalX, finalY;
+// instantiate the globals we need...I know...yuck!
+let x,
+    y,
+    isPainting,
+    origX,
+    origY,
+    finalX,
+    finalY;
 
 function getCoordinates(event) {
   if (['mousedown', 'mousemove'].includes(event.type)) {
@@ -66,14 +72,14 @@ function drawLine(firstX, firstY, secondX, secondY) {
 
 function paint(e) {
   if (isPainting) {
-    var [newX, newY] = getCoordinates(e);
+    const [newX, newY] = getCoordinates(e);
     drawLine(x, y, newX, newY);
 
     // save these for later
-    coordinateArray.push({
-      'x':newX,
-      'y':newY
-    });
+    coordinateArray.push([
+      newX,
+      newY
+    ]);
 
     // set x and y to our new coordinates
     finalX = x = newX;
@@ -86,17 +92,26 @@ function getSlope(startX, startY, endX, endY) {
   return slope.toFixed(2);
 }
 
+function generateLineFromLinear(coordinateArray) {
+  console.log(coordinateArray);
+  const centeredArray = coordinateArray.map((x, y) => {
+    return [x - fakeCenterX, y - fakeCenterY - y];
+  })
+  const centeredResult = regression.linear(centeredArray);
+  const result = regression.linear(coordinateArray);
+  console.log(result);
+  console.log(centeredResult);
+}
+
 function exit() {
 
   isPainting = false;
   const slope = getSlope(origX, origY, finalX, finalY);
 
-  console.log(coordinateArray);
+  generateLineFromLinear(coordinateArray);
 
   // deal with 0, 0 being at the top left of the viewport
-  const fakeCenterX = canvas.width/2;
   const fakeOrigX = origX - fakeCenterX;
-  const fakeCenterY = canvas.height/2;
   const fakeOrigY = fakeCenterY - origY;
 
   // we have 9 "units" each above/below the midpoint, so a bit hacky
